b564461fbbe30b2998b8d84a2cbbb6c85cb96a22
amina-wq <kannami2004@gmail.com>
2023-12-16 15:25:17 +0300

    main-loop logic changes

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from utils import login, show_menu
 
 if __name__ == "__main__":
     while True:
-        user = login()
-        show_menu(user)
+        if user := login():
+            show_menu(user)
+        else:
+            break
